0a7b615ee5dead2d91512f400b6383ad2f266679
Alican Balik <alicanbalik@outlook.com>
2016-10-08 18:30:17 +0200

    Modified Contact page

--- public/javascripts/contact.js
+++ public/javascripts/contact.js
@@ -1,12 +1,14 @@
 function buttonClick(event) {
+	
 	var nameSurname = document.getElementById("nameSurname");
 	var email = document.getElementById("email");
 	var message = document.getElementById("message");
 	var title = document.getElementById("title");
-    if (nameSurname.value.length == 0|| email.value.length == 0 || message.value.length == 0 || title.value.length == 0) {
+    var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+    if (nameSurname.value.length == 0 || email.value.length == 0 || message.value.length == 0 || title.value.length == 0 || !testEmail.test(email.value)) {
         event.preventDefault();
-        alert("Please fill out the form!");
+        alert("Please check the form!");
     } else {
-    	alert("You have successfully sent your mail. We will reply you back as fast as possible.")
+    	alert("You have successfully sent your mail. We will reply you back as fast as possible.");
     }
 }
